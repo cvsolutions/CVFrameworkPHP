d582f2d628ac077fbc0dbc16d6863efd1e7dc1aa
Cv Solutions <cvsolutions@me.com>
2013-01-08 14:36:46 +0100

    Update project

--- config.php
+++ config.php
@@ -1,8 +1,21 @@
 <?php
 
-ini_set('display_errors', 0);
+/**
+ * Enable display errors
+ */
+ini_set('display_errors', 1);
 
-define('DB_HOST', '');
-define('DB_USER', '');
-define('DB_PWD', '');
-define('DB_NAME', '');
+/**
+ * Database connection parameters
+ * @var unknown
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PWD', 'rootpass');
+define('DB_NAME', 'test');
+
+/**
+ * Password-protecting a secret key ...
+ * @var unknown
+ */
+define('SECRET_KEY', 'Cv50lu710n5');
--- libs/core/CV_Solutions.php
+++ libs/core/CV_Solutions.php
@@ -9,82 +9,76 @@ include_once 'CV_Model.php';
 
 /**
  * CV_Solutions
- * 
+ *
  * @author Concetto Vecchio
  * @copyright 2013
  * @version 1.0
  * @package PHP Framework
  * @license GNU General Public License 3 (http://www.gnu.org/licenses/)
  * @link http://cvsolutions.it
- * 
+ *
  */
 class CV_Solutions
 {
-	/**
-	 * Request
-	 * @return multitype:
-	 */
-	private function Request()
-	{
-		$uri = $_SERVER['REQUEST_URI'];
-		$params = explode('/', $uri);
-		return array_filter($params);
-	}
+    /**
+     * @return array
+     */
+    private function Request()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $params = explode('/', $uri);
+        return array_filter($params);
+    }
 
-	/**
-	 * Load_Controller
-	 * @param array $request REQUEST URI
-	 * @throws Exception
-	 * @return boolean
-	 */
-	private function Load_Controller(array $request)
-	{
-		$path = realpath(dirname(__FILE__) . '/../../controller');
-		
-		if(isset($request[1]))
-		{
-			$class = sprintf('%sController.php', $request[1]);
-			$fileName = $path . '/' . $class;
-			
-			if(is_readable($fileName))
-			{
-				// Common Controller
-				include_once $fileName;
-				$obj = new $request[1]();
-				$method = isset($request[2]) ? $request[2] : 'Index';
-				$obj->$method($arg = false);
-				return true;
-			}
-			
-		} else {
-			
-			// Default Controller
-			include_once $path . '/IndexController.php';
-			$Controller = new Index();
-			$Controller->Index();
-			return true;
-		}
-		
-		throw new Exception('The page you requested was not found');
-	}
-	
-	/**
-	 * Bootstrap
-	 * @return boolean
-	 */
-	public function Bootstrap()
-	{
-		try {
-				
-			$uri = $this->Request();
-			// print_r($uri);
-			$this->Load_Controller($uri);
-			return true;
-				
-		} catch (Exception $e)
-		{
-			// Error Request
-			exit($e->getMessage());
-		}
-	}
+    /**
+     * @param array $request obj
+     * @return bool
+     * @throws Exception
+     */
+    private function Load_Controller(array $request)
+    {
+        $path = realpath(dirname(__FILE__) . '/../../controller');
+
+        if (isset($request[1])) {
+            $class = sprintf('%sController.php', $request[1]);
+            $fileName = $path . '/' . $class;
+
+            if (is_readable($fileName)) {
+                // Common Controller
+                include_once $fileName;
+                $obj = new $request[1]();
+                $method = isset($request[2]) ? $request[2] : 'Index';
+                $obj->$method($arg = false);
+                return true;
+            }
+
+        } else {
+
+            // Default Controller
+            include_once $path . '/IndexController.php';
+            $Controller = new Index();
+            $Controller->Index();
+            return true;
+        }
+
+        throw new Exception('The page you requested was not found');
+    }
+
+    /**
+     * @return bool
+     */
+    public function Bootstrap()
+    {
+        try {
+
+            $uri = $this->Request();
+            // print_r($uri);
+            $this->Load_Controller($uri);
+            return true;
+
+        } catch (Exception $e) {
+            // Error Request
+            exit($e->getMessage());
+        }
+    }
 }
